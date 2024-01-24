6049030464e055b7a2c4caf2d3762a03cd053589
Arsen <arsenblade@mail.ru>
2024-01-24 20:13:55 +0500

    Добавление провайдера на страницы

---
+++ src/app/Providers/MainProvider/ui/MainProvider.tsx
@@ -0,0 +1,36 @@
+import React, {FC, useEffect} from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {Header} from 'widgets/Header';
+import AppRouter from '../../../Router/Router';
+import ErrorBoundary from '../../ErrorBoundary/ErrorBoundary';
+import {useActionCreatorsTyped} from 'shared/hooks/useActionsCreators';
+import {authActions} from 'entities/User';
+import styles from './MainProvider.module.scss';
+import {Navigation} from 'widgets/Navigation';
+
+interface MainProviderProps {
+    children?: React.ReactNode
+}
+
+const MainProvider:FC<MainProviderProps> = ({ children }) => {
+    const actionsAuth = useActionCreatorsTyped(authActions);
+
+    useEffect(() => {
+         actionsAuth.checkAuth().then();
+    }, []);
+
+    return (
+      <BrowserRouter>
+        <ErrorBoundary>
+          <Header />
+          <Navigation />
+          {children}
+          <main className={styles.main}>
+            <AppRouter />
+          </main>
+        </ErrorBoundary>
+      </BrowserRouter>
+    );
+};
+
+export default MainProvider;
